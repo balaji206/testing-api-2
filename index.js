87b64bdda8c38cff89b79a5371b27e6c7c5a003c
balaji <balajier2006@gmail.com>
2025-02-13 16:06:21 +0530

    done

--- index.js
+++ index.js
@@ -1,57 +1,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const data = require("./data.json")
-
-
+const data = require('./data.json');
 const app = express();
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.post("/books",(req,res)=>{
-  const {books_id,title,author,genre,year,copies} = req.body
-  console.log(req.body)
-  return res.status(200).json({sdfds:"ddf"})
+// In-memory book storage (Temporary, replace with DB later)
+// GET all books
+app.get('/books', (req, res) => {
+  console.log(data);
+  res.json(data);
+});
+app.get('/books/:id', (req, res) => {
+  const { id } = req.params;
+  const book = data.filter((book) => book.book_id === id);
+  res.json(book);
+});
 
-})
-app.get("/books/:id",(req,res)=>{ 
-  const bk_id = req.params.id
-  const bk = data.filter((book)=>{
-    return book.book_id == Number(bk_id)
-  })
-  return  res.status(200).json({book:bk})
-})
-
-
+// POST a new book
+app.post('/books', (req, res) => {
+  const book = req.body;
+  data.push(book);
+  res.json(book);
+});
 app.put('/books/:id',(req,res)=>{
-  const {id}=req.params
-  const {description}=req.body
-const book =data.filter(item=>id==item.book_id)
-if(book.length==0)
-  res.json('book not found')
-book[0]={...book, description}
-res.send(book[0])
+  const {id} = req.params
+  const {book_id,name,author,genre,yaer,copies} = req.body
+  const book = data.filter(book=>book.book_id == id)
 
+  if(book_id||title||author||genre||copies||)
+  {
+    return res.status(400).json({message:"please provide all details"})
+  }
+  if(idx==-1)
+  {
+    return res.status(400).json({message:"Book not found"})
+  }
+  data[idx]={
+    ...req.body
+  }
+  res.json(data[idx])
 })
-app.delete("/books/:id",(req,res)=>{
-  const bk_id = req.params.id
-  const bks = data.filter((book)=>{
-  return book.book_id != Number(bk_id)
-  })
-
-  return  res.status(200).json({books:bks})
-
-
-})
-app.get("/books",(req,res)=>{
-  return res.status(200).json({books:data})
-
-})
-
-
-
-
 app.listen(PORT, () => {
-
   console.log(`Server is running on port ${PORT}`);
 });
